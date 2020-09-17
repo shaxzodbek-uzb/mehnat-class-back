5eee6d84737d80ab68aeb9b006117f059fa35ff0
abdumalik995 <abdumalik.botirov.95@mail.ru>
2020-09-17 13:12:12 +0500

    tests for user api

--- tests/Feature/UserResourceTest.php
+++ tests/Feature/UserResourceTest.php
@@ -2,6 +2,7 @@
 
 namespace Tests\Feature;
 
+use Illuminate\Support\Str;
 use Illuminate\Foundation\Testing\RefreshDatabase;
 use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
@@ -13,10 +14,65 @@ class UserResourceTest extends TestCase
      *
      * @return void
      */
-    // public function testExample()
-    // {
-    //     $response = $this->get('/');
+    public function testExample()
+    {
+        $response = $this->get('/');
 
-    //     $response->assertStatus(200);
-    // }
+        $response->assertStatus(200);
+    }
+
+    public function testIndexUser()
+    {
+        $response = $this->json('GET', 'api/v1/users');
+
+        $response
+            ->assertStatus(200)
+            ->assertJson([
+                'success' => true,
+            ]);
+    }
+
+    public function testStoreUser()
+    {
+        $response = $this->json('POST', 'api/v1/users', [
+            'username' => 'Sally '.Str::random(3),
+            'fullname' => 'Smith',
+            'password' => bcrypt(123456),
+            'age' => 19
+        ]);
+       
+        $response
+            ->assertStatus(200)
+            ->assertJson([
+                'success' => true,
+            ]);
+    }
+
+    public function testShowUser()
+    {
+        $response = $this->json('GET', 'api/v1/users/7');
+
+        $response
+            ->assertStatus(200)
+            ->assertJson([
+                'success' => true,
+            ]);
+    }
+
+    public function testUpdateUser()
+    {
+        $response = $this->putJson('api/v1/users/7', [
+            'id' => 7,
+            'username' => 'Marin '.Str::random(3),
+            'fullname' => 'Barin',
+            'password' => bcrypt(123456),
+            'age' => 19
+        ]);
+       
+        $response
+            ->assertStatus(200)
+            ->assertJson([
+                'success' => true,
+            ]);
+    }
 }
